17a4fba3bc67e37274578544e6276a1eed0e38b9
Tran Ngoc Thanh <thanhbk113@gmail.com>
2022-06-26 20:59:10 +0700

    EC-39 [HOT][SUPER] BUG LOGIN ADMIN

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,127 +1,113 @@
-import axios from 'axios';
-import { BASE_URL_API, ERROR_TOKEN, ROUTES } from 'constant';
-import jwt_decode, { JwtPayload } from 'jwt-decode';
-import NextAuth from 'next-auth';
-import { JWT } from 'next-auth/jwt';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import { IResLogin, IToken } from 'shared/types';
+import axios from "axios";
+import { BASE_URL_API, ERROR_TOKEN, ROUTES } from "constant";
+import jwt_decode, { JwtPayload } from "jwt-decode";
+import NextAuth from "next-auth";
+import { JWT } from "next-auth/jwt";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { IResLogin, IToken } from "shared/types";
 
 const handleRefreshToken = async (token: JWT) => {
-   try {
-      const tokenData: IToken = await axios
-         .post(`${BASE_URL_API}/auth/refresh-token`, {
-            refresh_token: token.refreshToken,
-         })
-         .then((value) => value.data.data);
+  try {
+    const tokenData: IToken = await axios
+      .post(`${BASE_URL_API}/auth/refresh-token`, {
+        refresh_token: token.refreshToken,
+      })
+      .then((value) => value.data.data);
 
-      const { access_token: accessToken, refresh_token: refreshToken } =
-         tokenData;
-      const accessTokenExpirationTime =
-         (jwt_decode<JwtPayload>(accessToken).exp as number) * 1000 - 10;
-      return {
-         ...token,
-         accessToken,
-         accessTokenExpires: accessTokenExpirationTime,
-         refreshToken: refreshToken ?? token.refreshToken, // Fall back to old refresh token
-      };
-   } catch (error) {
-      return {
-         ...token,
-         error: ERROR_TOKEN,
-      };
-   }
+    const { access_token: accessToken, refresh_token: refreshToken } =
+      tokenData;
+    const accessTokenExpirationTime =
+      (jwt_decode<JwtPayload>(accessToken).exp as number) * 1000 - 10;
+    return {
+      ...token,
+      accessToken,
+      accessTokenExpires: accessTokenExpirationTime,
+      refreshToken: refreshToken ?? token.refreshToken, // Fall back to old refresh token
+    };
+  } catch (error) {
+    return {
+      ...token,
+      error: ERROR_TOKEN,
+    };
+  }
 };
 
 export default NextAuth({
-   // Configure one or more authentication providers
-   providers: [
-      CredentialsProvider({
-         name: 'Credentials',
-         credentials: {
-            email: {
-               label: 'Email',
-               type: 'email',
-               placeholder: 'email@domain.com',
-            },
-            password: { label: 'Password', type: 'password' },
-         },
-         //@ts-ignore
-         authorize: async (credentials) => {
-            try {
-               //login
+  // Configure one or more authentication providers
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "email@domain.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      //@ts-ignore
+      authorize: async (credentials) => {
+        try {
+          //login
 
-               const data: IResLogin = await axios
-                  .post(`${BASE_URL_API}/auth/login`, {
-                     email: credentials?.email,
-                     password: credentials?.password,
-                     role: 'admin',
-                  })
-                  .then((value) => {
-                     return value.data.data;
-                  });
+          const data: IResLogin = await axios
+            .post(`${BASE_URL_API}/auth/login`, {
+              email: credentials?.email,
+              password: credentials?.password,
+              role: "admin",
+            })
+            .then((value) => {
+              return value.data.data;
+            });
 
-               if (data) {
-                  // neu co data
-                  const {
-                     access_token: accessToken,
-                     refresh_token: refreshToken,
-                  } = data.token;
+          if (data) {
+            const { access_token: accessToken, refresh_token: refreshToken } =
+              data.token; //We get the access token and the refresh token from the data object.
 
-                  const accessTokenExpirationTime =
-                     (jwt_decode<JwtPayload>(accessToken).exp as number) *
-                        1000 -
-                     10; // parse tk token ra de lay cai gia tri het han * 1000 de lay ms
+            const accessTokenExpirationTime =
+              (jwt_decode<JwtPayload>(accessToken).exp as number) * 1000 - 10;
+            //minus 10 seconds before expiration time to prevent token expiration error in the browser side unit ms
 
-                  return {
-                     ...data.user,
-                     accessToken,
-                     accessTokenExpires: accessTokenExpirationTime,
-                     refreshToken,
-                  };
-               }
-               return null;
-            } catch (e: any) {
-               throw new Error(e.response.data.message);
-            }
-         },
-      }),
-   ],
-   callbacks: {
-      async jwt({ token, user }) {
-         // console.log(user);
-         // console.log('user::', user);
-         if (token) {
-            const {
-               accessToken,
-               accessTokenExpires,
-               refreshToken,
-               ...userData
-            } = token;
             return {
-               accessToken,
-               accessTokenExpires,
-               refreshToken,
-               user: userData,
+              ...data.user,
+              accessToken,
+              accessTokenExpires: accessTokenExpirationTime,
+              refreshToken,
             };
-         }
-         // get time => tg hien tai theo kieu ms check thu neu ma thoi gian hien tai nho hon cai thoi gian het han thi k lam di het :)
-         // @ts-ignore
-         if (new Date().getTime() < token.accessTokenExpires) {
-            return token;
-         }
-         // refresh token here
-         // nguoc lai thi refresh token
-         return await handleRefreshToken(token);
+            //return new object user contain token
+          }
+          return null; //if the data is null, return null
+        } catch (e: any) {
+          throw new Error(e.response.data.message); //if the server response is an error, throw an error with the message from the server
+        }
       },
-      async session({ session, token }) {
-         //@ts-ignore
-         session.user = token.user;
-         session.accessToken = token.accessToken;
-         session.error = token.error;
-         return session;
-      },
-   },
-   pages: {
-      signIn: ROUTES.LOGIN,
-   },
+    }),
+  ],
+  callbacks: {
+    //The jwt() callback is called when a new token is created.
+    async jwt({ token, user }) {
+      if (user) {
+        return {
+          ...token,
+          user,
+        };
+      }
+      return token;
+    },
+    //The session() callback is called when a user logs in or log out
+    async session({ session, token }) {
+      //@ts-ignore
+      if (session) {
+        return {
+          ...session,
+          token,
+        };
+      }
+      return session;
+    },
+  },
+  //The signIn page is the page that the user is redirected to when they are not logged in.
+  pages: {
+    signIn: ROUTES.LOGIN,
+  },
 });
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,21 @@
-import { Auth, Layout } from 'components';
-import { NextPage } from 'next';
-import React from 'react';
-import { WithLayout } from 'shared/types';
+import { Auth, Layout } from "components";
+import { NextPage } from "next";
+import { getSession } from "next-auth/react";
+import React from "react";
+import { WithLayout } from "shared/types";
 
 const Home: NextPage & WithLayout = () => {
-   return <div>hi</div>;
+  return <div>hi</div>;
 };
 
 export default Home;
 
 Home.getLayout = (page) => {
-   return (
-      <Auth>
-         <Layout>{page}</Layout>
-      </Auth>
-   );
+  const session = getSession();
+  console.log("session", session);
+  return (
+    <Auth>
+      <Layout>{page}</Layout>
+    </Auth>
+  );
 };
